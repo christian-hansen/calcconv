33ebc2a3f5e30a54022816a27fff3cc90abe1dbe
christian-hansen <mail@hansen-net.de>
2023-02-02 11:07:51 +0100

    addec calculator

--- js/calculator.js
+++ js/calculator.js
@@ -3,7 +3,7 @@ function display(val) {
     document.getElementById("result").value += val;
 }
 
-
+//Function that gets the values based on keyboard inputs
 function type(event) {
     if (event.key == '0' || event.key == '1' 
         || event.key == '2' || event.key == '3'
@@ -15,7 +15,14 @@ function type(event) {
         document.getElementById("result").value += event.key;
 }
 
-// Function that clear the display
+// Function that evaluates the digit and return result
+function solve() {
+    let input = document.getElementById("result").value;
+    result = eval(input);
+    document.getElementById("result").value = result;
+}
+
+// Function that clears the display
 function reset() {
     document.getElementById("result").value = "";
 }
