c31e824b5b222c8d21ba652bf0483a98a8d51ffd
christian-hansen <mail@hansen-net.de>
2023-02-02 10:30:32 +0100

    minor change

---
+++ js/calculator.js
@@ -0,0 +1,21 @@
+// Function that display value
+function display(val) {
+    document.getElementById("result").value += val;
+}
+
+
+function type(event) {
+    if (event.key == '0' || event.key == '1' 
+        || event.key == '2' || event.key == '3'
+        || event.key == '4' || event.key == '5' 
+        || event.key == '6' || event.key == '7'
+        || event.key == '8' || event.key == '9' 
+        || event.key == '+' || event.key == '-'
+        || event.key == '*' || event.key == '/')
+        document.getElementById("result").value += event.key;
+}
+
+// Function that clear the display
+function reset() {
+    document.getElementById("result").value = "";
+}
